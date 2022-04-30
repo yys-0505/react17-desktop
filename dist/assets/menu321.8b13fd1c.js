b0deae8d2b223daed303b21e6dc7e48796da5547
yishuai.yin@accenture.com <yishuai.yin@accenture.com>
2022-04-30 22:57:17 +0800

    new prod build

--- dist/assets/menu321.8b13fd1c.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./index.c17553aa.js";var s="C:\\Users\\yishuai.yin\\Desktop\\exercise\\react\\react17-desktop\\src\\pages\\menu3\\menu321\\menu321.jsx";const i=()=>e.exports.jsxDEV("div",{children:"menu3-2-1"},void 0,!1,{fileName:s,lineNumber:4,columnNumber:10},globalThis);export{i as default};
