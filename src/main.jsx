21801b3aedf9c81429aa8c9bfba246d7756d53f2
yishuai.yin@accenture.com <yishuai.yin@accenture.com>
2022-04-30 22:55:50 +0800

    try to fix redux console error

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { FullPageLoading } from 'components/full-page-loading'
 import App from './App'
 import 'common/less/index.less'
 
+import.meta.env.PROD && (process.env.NODE_ENV = 'production')
+
 const Entry = (
   // <React.StrictMode>
     <Provider store={store}>
