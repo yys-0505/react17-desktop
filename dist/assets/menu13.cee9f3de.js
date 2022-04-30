8451bc9568ec443268801705a1f7b7a6974cf815
yishuai.yin@accenture.com <yishuai.yin@accenture.com>
2022-04-30 21:17:07 +0800

    change baseURL

---
+++ dist/assets/menu13.cee9f3de.js
@@ -0,0 +1,1 @@
+import{O as e}from"./index.c17553aa.js";var s="C:\\Users\\yishuai.yin\\Desktop\\exercise\\react\\react17-desktop\\src\\pages\\menu1\\menu13\\menu13.jsx";const i=()=>e.exports.jsxDEV("div",{children:"menu1-3"},void 0,!1,{fileName:s,lineNumber:4,columnNumber:10},globalThis);export{i as default};
---
+++ dist/assets/menu4.ecfd8bbd.js
@@ -0,0 +1,1 @@
+import{O as e}from"./index.c17553aa.js";var s="C:\\Users\\yishuai.yin\\Desktop\\exercise\\react\\react17-desktop\\src\\pages\\menu4\\menu4.jsx";const i=()=>e.exports.jsxDEV("div",{children:"menu4"},void 0,!1,{fileName:s,lineNumber:4,columnNumber:10},globalThis);export{i as default};
